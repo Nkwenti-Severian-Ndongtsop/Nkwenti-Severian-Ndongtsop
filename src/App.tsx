5df35ef6f43ec80deae2a3478265805deb2d0c54
Nkwenti-Severian-Ndongtsop <halamadrid651643565@gmail.com>
2025-07-31 09:16:55 +0100

    chore: remove Admin route from App component

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import Index from "./pages/Index";
 import Projects from "./pages/Projects";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import Admin from "./pages/Admin";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -23,7 +22,6 @@ const App = () => (
           <Route path="/projects" element={<Projects />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/admin" element={<Admin />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
