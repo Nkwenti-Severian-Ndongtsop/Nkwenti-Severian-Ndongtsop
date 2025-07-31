66afac8a52d589f457b81b0f8dcf327c0f511ab9
Nkwenti-Severian-Ndongtsop <halamadrid651643565@gmail.com>
2025-07-31 13:27:45 +0100

    fix: update live URL in Index component to reflect new deployment location

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -43,7 +43,7 @@ const Index = () => {
       description: "An AI-powered portfolio website with a chatbot",
       image: "/portfolio-ai.png",
       technologies: ["React", "TypeScript", "Tailwind CSS", "Node.js", "Docker","GitHub","Groq"],
-      liveUrl: "https://portfolio-ai-server.onrender.com/",
+      liveUrl: "https://frontend-portfolio-htbj.onrender.com/",
       githubUrl: "https://github.com/Nkwenti-Severian-Ndongtsop/Nkwenti-Severian-Ndongtsop.git",
       category: "AI/ML",
       featured: true,
