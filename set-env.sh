6440d8016a338776d5e88fb3427e5bedab80eb3f
Nkwenti-Severian-Ndongtsop <halamadrid651643565@gmail.com>
2025-08-14 07:55:16 +0100

    remove uneccessary files

--- set-env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-# Replace the placeholder in env.js with the actual env variable
-if [ -n "$VITE_AI_SERVER_URL" ]; then
-  sed -i "s|__VITE_AI_SERVER_URL__|$VITE_AI_SERVER_URL|g" /usr/share/nginx/html/env.js
-fi
-
-exec nginx -g "daemon off;"
